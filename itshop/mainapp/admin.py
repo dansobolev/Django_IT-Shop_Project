38f68df547bef79f9101bd2a53746adff692eec2
dansobolev <daniil.sob56@gmail.com>
2020-12-21 04:23:58 +0300

    register tables to the admin panel

--- itshop/mainapp/admin.py
+++ itshop/mainapp/admin.py
@@ -1,3 +1,41 @@
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class NotebookCategoryChoiceField(forms.ModelChoiceField):
+
+    pass
+
+
+# необходимо для того, чтобы в панели админа при попытке добавления нового ноутбука
+# при выборе категории не вылезала категория со смартфонами
+class NotebookAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return NotebookCategoryChoiceField(Category.objects.filter(slug='notebooks'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class SmartphoneCategoryChoiceField(forms.ModelChoiceField):
+
+    pass
+
+
+# та же самая проверка, как и в случае с ноутбками
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return SmartphoneCategoryChoiceField(Category.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
