0365514db33df1f07704f58786b433602d346820
dansobolev <daniil.sob56@gmail.com>
2020-12-23 03:03:58 +0300

    added main url to handle basic pages

--- itshop/itshop/urls.py
+++ itshop/itshop/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('mainapp.urls')),
 ]
+
+# проверка на то, что при DEBUG=True мы берем статик файлы из наших директорий
+# на проде они будут храниться где-то на другом сервере (напр. на NGINX)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
